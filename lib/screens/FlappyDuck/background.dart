63b22158d78e0583886fe178d2950197472a114d
Marek Špirka <marek.spirka17@gmail.com>
2022-12-12 15:41:40 +0100

    Edited background Flappy Duck

--- lib/screens/FlappyDuck/background.dart
+++ lib/screens/FlappyDuck/background.dart
@@ -5,9 +5,12 @@ class Brno extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.blue,
-    );
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background_flappy.png'),
+              fit: BoxFit.cover),
+        ));
   }
 }
